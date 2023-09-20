cd59cb7d6c1e68b406c6ba88675bea382c0073fc
Nazar Bulyha <nbulyha@fieldcomplete.com>
2023-09-20 15:49:56 +0300

    Modified gitignore to not to commit non-related items. Modified restore file to use all needed files from the Backups.

--- restore.sql
+++ restore.sql
@@ -1,29 +1,103 @@
-RESTORE DATABASE V3_DEV_FC00018
-FROM DISK = '/var/opt/mssql/backup/STAGE_FC00018.bak'
-WITH MOVE 'FC00018' TO '/var/opt/mssql/data/V3_DEV_FC00018.mdf',
-     MOVE 'FC00018_log' TO '/var/opt/mssql/data/V3_DEV_FC00018_log.ldf';
+-- V3_DEV_FC00018
 
-RESTORE DATABASE V3_DEV_FC00019
-FROM DISK = '/var/opt/mssql/backup/STAGE_FC00019.bak'
-WITH MOVE 'FC00019' TO '/var/opt/mssql/data/V3_DEV_FC00019.mdf',
-     MOVE 'FC00019_log' TO '/var/opt/mssql/data/V3_DEV_FC00019_log.ldf';
+ALTER DATABASE [V3_DEV_FC00018] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 
-RESTORE DATABASE V3_DEV_FC00020
-FROM DISK = '/var/opt/mssql/backup/STAGE_FC00020.bak'
-WITH MOVE 'FC00020' TO '/var/opt/mssql/data/V3_DEV_FC00020.mdf',
-     MOVE 'FC00020_log' TO '/var/opt/mssql/data/V3_DEV_FC00020_log.ldf';
+RESTORE DATABASE [V3_DEV_FC00018]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FC00018.bak'
+WITH FILE = 1,
+MOVE N'FC00018' TO N'/var/opt/mssql/data/V3_DEV_FC00018.mdf',
+MOVE N'FC00018_log' TO N'/var/opt/mssql/data/V3_DEV_FC00018_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
 
-RESTORE DATABASE V3_DEV_FieldCompleteIdentity
-FROM DISK = '/var/opt/mssql/backup/STAGE_FieldCompleteIdentity.bak'
-WITH MOVE 'IdentityDevelopment' TO '/var/opt/mssql/data/V3_DEV_FieldCompleteIdentity.mdf',
-     MOVE 'IdentityDevelopment_log' TO '/var/opt/mssql/data/V3_DEV_FieldCompleteIdentity_log.ldf';
+ALTER DATABASE [V3_DEV_FC00018] SET MULTI_USER
 
-RESTORE DATABASE V3_DEV_FieldCompleteMarketplace
-FROM DISK = '/var/opt/mssql/backup/STAGE_FieldCompleteMarketplace.bak'
-WITH MOVE 'FieldCompleteMarketplace' TO '/var/opt/mssql/data/V3_DEV_FieldCompleteMarketplace.mdf',
-     MOVE 'FieldCompleteMarketplace_log' TO '/var/opt/mssql/data/V3_DEV_FieldCompleteMarketplace_log.ldf';
+-- V3_DEV_FC00019
 
-RESTORE DATABASE V3_DEV_FieldCompleteReport
-FROM DISK = '/var/opt/mssql/backup/STAGE_FieldCompleteReport.bak'
-WITH MOVE 'FieldCompleteReport' TO '/var/opt/mssql/data/V3_DEV_FieldCompleteReport.mdf',
-     MOVE 'FieldCompleteReport_log' TO '/var/opt/mssql/data/V3_DEV_FieldCompleteReport_log.ldf';
+ALTER DATABASE [V3_DEV_FC00019] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FC00019]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FC00019.bak'
+WITH FILE = 1,
+MOVE N'FC00019' TO N'/var/opt/mssql/data/V3_DEV_FC00019.mdf',
+MOVE N'FC00019_log' TO N'/var/opt/mssql/data/V3_DEV_FC00019_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FC00019] SET MULTI_USER
+
+-- V3_DEV_FC00020
+
+ALTER DATABASE [V3_DEV_FC00020] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FC00020]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FC00020.bak'
+WITH FILE = 1,
+MOVE N'FC00020' TO N'/var/opt/mssql/data/V3_DEV_FC00020.mdf',
+MOVE N'FC00020_log' TO N'/var/opt/mssql/data/V3_DEV_FC00020_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FC00020] SET MULTI_USER
+
+-- V3_DEV_FieldCompleteIdentity
+
+ALTER DATABASE [V3_DEV_FieldCompleteIdentity] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FieldCompleteIdentity]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FieldCompleteIdentity.bak'
+WITH FILE = 1,
+MOVE N'IdentityDevelopment' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteIdentity.mdf',
+MOVE N'IdentityDevelopment_log' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteIdentity_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FieldCompleteIdentity] SET MULTI_USER
+
+-- V3_DEV_FieldCompleteReport
+
+ALTER DATABASE [V3_DEV_FieldCompleteReport] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FieldCompleteReport]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FieldCompleteReport.bak'
+WITH FILE = 1,
+MOVE N'FieldCompleteReport' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteReport.mdf',
+MOVE N'FieldCompleteReport_log' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteReport_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FieldCompleteReport] SET MULTI_USER
+
+-- V3_DEV_FieldCompleteStats
+
+ALTER DATABASE [V3_DEV_FieldCompleteStats] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FieldCompleteStats]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FieldCompleteStats.bak'
+WITH FILE = 1,
+MOVE N'V3_DEV_FieldCompleteStats' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteStats.mdf',
+MOVE N'V3_DEV_FieldCompleteStats_log' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteStats_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FieldCompleteStats] SET MULTI_USER
+
+-- V3_DEV_FieldCompleteMailer
+
+ALTER DATABASE [V3_DEV_FieldCompleteMailer] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FieldCompleteMailer]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FieldCompleteMailer.bak'
+WITH FILE = 1,
+MOVE N'FieldCompleteMailer' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteMailer.mdf',
+MOVE N'FieldCompleteMailer_log' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteMailer_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FieldCompleteMarketplace] SET MULTI_USER
+
+-- V3_DEV_FieldCompleteMarketplace
+
+ALTER DATABASE [V3_DEV_FieldCompleteMarketplace] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+
+RESTORE DATABASE [V3_DEV_FieldCompleteMarketplace]
+FROM DISK = N'/var/opt/mssql/backup/V3_DEV_FieldCompleteMarketplace.bak'
+WITH FILE = 1,
+MOVE N'FieldCompleteMarketplace' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteMarketplace.mdf',
+MOVE N'FieldCompleteMarketplace_log' TO N'/var/opt/mssql/data/V3_DEV_FieldCompleteMarketplace_log.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+
+ALTER DATABASE [V3_DEV_FieldCompleteMarketplace] SET MULTI_USER
--- start_and_restore.sh
+++ start_and_restore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start SQL Server
-/opt/mssql/bin/sqlservr & 
+/opt/mssql/bin/sqlservr &
 
 # Allow SQL Server to start
-sleep 30 
+sleep 30
 
 # Restore databases
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i /var/opt/mssql/backup/restore.sql
